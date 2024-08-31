18db9927486cf114dbe90a7238c8e80f7fd98b76
karan vig <karanvig35@gmail.com>
2024-08-31 12:19:22 +0530

    added the secret key

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -13,7 +13,8 @@ const handler= NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
           }),
-    ]
+    ],
+    secret: process.env.NEXT_AUTH_URL
 })
 
 export {handler as GET,handler as POST}
